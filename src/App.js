4ab3c5ce3e73ead191989aa0978cf4ab29e18263
Jasper <jasper_hendrickx@hotmail.com>
2021-08-12 21:42:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { FormControl, Button, Table, InputGroup } from 'react-bootstrap';
+import FormContainer from './components/FormContainer';
+
+const feedback = [
+  {
+    category: 'pronunciation',
+    mainText: [
+      'text when bad',
+      'text when slightly less bad',
+      'text when almost passable',
+      'text when average',
+      'text when good',
+      'text when excellent',
+    ],
+    commonErrors: {
+      name: 'th',
+      text: 'Please work on your th-sound',
+    },
+  },
+  {
+    category: 'fluency',
+    mainText: [
+      'text when bad',
+      'text when slightly less bad',
+      'text when almost passable',
+      'text when average',
+      'text when good',
+      'text when excellent',
+    ],
+    commonErrors: {
+      name: 'reading',
+      text: 'Stop reading and speak naturally',
+    },
+  },
+  {
+    category: 'grammar',
+    mainText: [
+      'text when bad',
+      'text when slightly less bad',
+      'text when almost passable',
+      'text when average',
+      'text when good',
+      'text when excellent',
+    ],
+    commonErrors: {
+      name: 'simple present',
+      text: 'You have to conjugate your tenses',
+    },
+  },
+];
+
+// user should be able to create new categories
+// user should be able to change amount of total points and consequently number of feedback texts = push more texts to array
+// user should be able to change specific feedback texts
+
+// want to change feedbackText 2 of fluency
+//location is feedback.category.maintext[i] (what if want to change commonErrors with same function?)
+// i = grade for that category
+// mainText = sub division of different feedback collections for each category
+// function changeOneItem(newValue) {
+
+//   return (feedback.fluency.mainText[3] = newValue);
+// }
+
+// feedback.position = feedback.newValue
+
+// console.log(feedback);
+// console.log(changeOneItem('5'));
+// console.log(feedback);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormContainer>
+      {feedback.map((feedbackItem) => (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <h4>{feedbackItem.category.toUpperCase()}</h4>
+            </tr>
+          </thead>
+          <tbody>
+            {feedbackItem.mainText.map((mainText, index) => (
+              <tr>
+                <InputGroup>
+                  <InputGroup.Text>{index}/5</InputGroup.Text>
+                  <FormControl
+                    as="textarea"
+                    aria-label=""
+                    placeholder={mainText}
+                  />
+                </InputGroup>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ))}
+      <Button size="lg">Update</Button>
+    </FormContainer>
   );
 }
 
---
+++ src/components/FeedbackInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FeedbackInput = () => {
+  const feedback = {
+    category: 'pronunciation',
+    feedback: [
+      'It seems like you are reading a Youdao translation. You are mispronouncing many words and you seem to have no idea what these words mean.',
+      'Your pronunciation is very hard or impossible to understand by a native speaker. Please review the pronunciation drills.',
+      'Your pronunciation is hard to understand by a native speaker. Please review the pronunciation drills.',
+      'Your pronunciation is clear enough. However, you have to pay more attention to common pronunciation issues such as th and v.',
+      'You speak clearly with good intonation, but you should still work more on common issues such as th and v.',
+      'Your pronunciation is accurate, with appropriate intonation, numbers of syllables and other correct nuances of pronunciation.',
+    ],
+  };
+
+  return <div></div>;
+};
+
+export default FeedbackInput;
